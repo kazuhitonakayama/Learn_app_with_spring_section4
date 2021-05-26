623c3683fe0043d13ee35f113d5dbdc289955583
kazuhitonakayama <firstjin19990101@gmail.com>
2021-05-26 18:33:43 +0900

    initial commit

---
+++ src/main/java/com/example/demo/LearnAppWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAppWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAppWithSpringApplication.class, args);
+	}
+
+}
